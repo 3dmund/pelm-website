3b04d557e43bb9a962c3f386b84c7d55a33c9f76
Edmund Tian <edmundtian@ezt.local>
2021-11-18 17:09:33 -0500

    add hamburger menu to mobile

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@
 		$("#without-pelm-summary").hide()
 	})
 })(jQuery);
+
+$(function(){
+	$("#header1").load("header.html");
+});
+
+// HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}
